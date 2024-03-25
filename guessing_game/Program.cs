5e778f9b927d72902e85eb786db64d94bcc1e302
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-25 14:11:58 +0200

    guessing game for fun using c#

---
+++ guessing_game/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class GuessGame
+{
+    static void Main()
+    {
+        string[] secretNames =  {"UCT", "UJ", "UKZN", "DUT", "Wits", "UP", "SU", "UWC", "UNISA", "NWU", "UL", "UFH", "UFS", "CUT", "TUT", "VUT", "NMMU", "Rhodes", "SMU", "CPUT", "MUT", "WSU"};
+        string guess;
+        int guessCount = 0;
+        int correctGuess = 0;
+        int guessLimit = 3;
+
+        Console.WriteLine("Welcome To The University Guessing Game");
+        Console.WriteLine("");
+        Console.WriteLine("Guess the name of a South African University you Love");
+        Console.WriteLine("");
+        Console.WriteLine("Note that the names are in a abbreviated form");
+        Console.WriteLine("");
+        Console.WriteLine("You have 3 chances to guess the correct name");
+        Console.WriteLine("");
+        Console.WriteLine("Good Luck");
+
+        while (guessCount < guessLimit)
+        {
+            Console.WriteLine("Enter your guess: ");
+            guess = Console.ReadLine();
+            guessCount++;
+
+            if (secretNames.Contains(guess))
+            {
+                correctGuess++;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect guess. Try again.");
+            }
+            if (correctGuess >= 3)
+            {
+                Console.WriteLine("Congratulations! You have guessed the correct name of a university in South Africa.");
+                break;
+            }
+
+        }
+
+        if (correctGuess < 3)
+        {
+            Console.WriteLine("You have exhausted your chances. The correct names of universities in South Africa are: ");
+            
+            for (int i = 0; i < secretNames.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {secretNames[i]}");
+            }
+
+        }
+    }
+}
+
+
+
